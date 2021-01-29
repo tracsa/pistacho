ae1b4ef3a3305ffaf1f7fa0b18f6337aa7fe0b7d
Kevin Villalobos <dracojan_12@hotmail.com>
2021-01-28 23:29:04 -0600

    Minor edit in jumbotron

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div id="app">
     <div class="jumbotron">
       <h1 class="display-4">Pistacho</h1>
-      <p class="lead"
-      >A simple process builder for <span class="font-weight-bold">Cacahuate</span>
+
+      <hr class="my-4"/>
+
+      <p class="lead">A simple process builder for <span class="font-weight-bold">Cacahuate</span>
 
         <a :href="cacahuateHomeUrl" class="mx-1">
           <font-awesome-icon :icon="['fas', 'home']"/>
